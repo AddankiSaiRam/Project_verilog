1d11944400a7daab33982a9c1534fc3891dd02f9
AddankiSaiRam <162308093+AddankiSaiRam@users.noreply.github.com>
2024-03-06 15:19:03 +0530

    Update 4x1_mux.v

--- 4x1_mux.v
+++ 4x1_mux.v
@@ -2,9 +2,9 @@ module m41 ( input a,
 input b, 
 input c, 
 input d, 
-input s0, s1,
+input [1:0]sel,
 output out); 
 
- assign out = s1 ? (s0 ? d : c) : (s0 ? b : a); 
+ assign out = sel[1] ? (sel[0] ? d : c) : (sel[0] ? b : a); 
 
 endmodule
